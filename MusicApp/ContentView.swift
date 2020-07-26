6fd2ff595eca8f831d7bda565352cc1393abe2c7
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2020-07-26 16:56:52 -0300

    Initial Commit

---
+++ MusicApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MusicApp
+//
+//  Created by Lisandro on 26/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Lisandro on 26/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
